920a3bc10cab08d070973c9cbcab50930d9d24af
Lositha <asakahatapitiya@gmail.com>
2020-03-29 01:25:46 +0530

    Initial commit

---
+++ src/main/java/lk/rupavahini/PPUManagement/PpuManagementApplication.java
@@ -0,0 +1,13 @@
+package lk.rupavahini.PPUManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpuManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpuManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/rupavahini/PPUManagement/PpuManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.rupavahini.PPUManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpuManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
